1fd5258518d43611e394db3d017261fb521b2fe3
Petko <Petko@HONOR>
2024-02-11 18:54:48 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Serves
+    {
+        string ser;
+        string log;
+        string pas;
+
+        public Serves(string ser, string log, string pas)
+        {
+            this.ser = ser;
+            this.log = log;
+            this.pas = pas;
+        }
+
+        public Serves(string[] str)
+        {
+            this.ser = str[0];
+            this.log = str[1];
+            this.pas = str[2];
+        }
+
+        public Serves() { }
+
+        public string Ser { get { return ser; } set { ser = value; } }
+        public string Log { get { return log; } set { log = value; } }
+        public string Pas { get { return pas; } set {  pas = value; } }
+
+        public override string ToString()
+        {
+            return ser + " " + log + " " + pas;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = "file.txt";
+
+        public MainWindow()
+        {
+            string str;
+            InitializeComponent();
+
+            if(File.Exists(path))
+            {
+                using(StreamReader file = new StreamReader(path))
+                {
+                    str = file.ReadToEnd();
+                }
+                string[] lines = str.Split('\n');
+                for(int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i] != "")
+                    {
+                        string[] strings = lines[i].Split();
+                        masPas.Items.Add(new Serves(strings));
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(textSer.Text != "" && textLog.Text != "" && textPas.Text != "")
+            {
+                masPas.Items.Add(new Serves(textSer.Text, textLog.Text, textPas.Text));
+
+                textSer.Text = "";
+                textLog.Text = "";
+                textPas.Text = "";
+            }
+            else
+            {
+                if(textSer.Text == "")
+                {
+                    textSer.Background = Brushes.Red;
+                }
+                if (textLog.Text == "")
+                {
+                    textLog.Background = Brushes.Red;
+                }
+                if (textPas.Text == "")
+                {
+                    textPas.Background = Brushes.Red;
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter file = new StreamWriter(path))
+            {
+                foreach(Serves serves in masPas.Items)
+                {
+                    file.WriteLine(serves);
+                }
+            }
+        }
+    }
+}
